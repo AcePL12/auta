d3b30066be57d9016fb9b6454f5ee80d5fd2953a
Dominik Szwed <ace19312@gmail.com>
2022-08-04 20:34:01 +0200

    poprawa tabeli

--- resources/views/manufacturers.blade.php
+++ resources/views/manufacturers.blade.php
@@ -1,5 +1,7 @@
+<head>
+</head>
 <h1>Manufacturer list</h1>
-<table border="1"> 
+<table border="2"> 
     <tr> 
         <td> Country </td>
         <td> Mfr_CommonName </td>
@@ -8,12 +10,19 @@
         <td> VehicleTypes </td>
     </tr>
     @foreach($collection as $item)
-    <tr> 
+    <tr>
         <td> {{$item['Country']}} </td>
         <td> {{$item['Mfr_CommonName']}} </td>
         <td> {{$item['Mfr_ID']}} </td>
         <td> {{$item['Mfr_Name']}} </td>
-        <td> </td>
+        @foreach($item['VehicleTypes'] as $type)
+        <td> 
+          <table >
+            <tr> <td> IsPrimary </td> <td> Name </td> </tr>
+            <tr> <td> {{$type['IsPrimary']}} </td> <td> {{$type['Name']}} </td> </tr>
+            </table>
+        </td>
+        @endforeach
     </tr>
     @endforeach
 </table>
